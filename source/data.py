c19a73986a5ebaa7307973842043f4ba66d6016e
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2022-02-28 22:20:16 -0800

    Update data.py

--- source/data.py
+++ source/data.py
@@ -5,6 +5,8 @@ import logging.config
 from pathlib import Path
 import click
 from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
 import numpy as np
 
 import warnings
@@ -39,7 +41,7 @@ def extract():
     
     logger.info(f"Done processing credit data.")
 
-    output_file = os.path.join(project_directory, 'data/raw/credit_data.pkl')
+    output_file = os.path.join(project_directory, 'data/raw/credit.pkl')
     logger.info(f"Saving credit data to `{output_file}`")
     credit_data.to_pickle(output_file)
 
@@ -48,6 +50,44 @@ def extract():
 def transform():
     logger.info(f"Transforming Data")
 
+@main.command()
+def create_training_test():
+    logger.info(f"Splitting training & test datasets")
+
+    with open(os.path.join(project_directory, 'data/raw/credit.pkl'), 'rb') as handle:
+        credit_data = pickle.load(handle)
+
+    y_full = credit_data['target']
+    X_full = credit_data.drop(columns='target')
+
+    # i.e. value of 0 is 'good' i.e. 'not default' and value of 1 is bad and what
+    # we want to detect i.e. 'default'
+    y_full = label_binarize(y_full, classes=['good', 'bad']).flatten()
+
+    X_train, X_test, y_train, y_test = train_test_split(X_full, y_full, test_size=0.2, random_state=42)
+
+    output_file = os.path.join(project_directory, 'data/raw/credit.pkl')
+
+
+    file_name = 'data/processed/X_train.pkl'
+    logger.info(f"Creating `{file_name}`")
+    with open(os.path.join(project_directory, file_name), 'wb') as handle:
+        pickle.dump(X_train, handle)
+
+    file_name = 'data/processed/X_test.pkl'
+    logger.info(f"Creating `{file_name}`")
+    with open(os.path.join(project_directory, file_name), 'wb') as handle:
+        pickle.dump(X_test, handle)
+
+    file_name = 'data/processed/y_train.pkl'
+    logger.info(f"Creating `{file_name}`")
+    with open(os.path.join(project_directory, file_name), 'wb') as handle:
+        pickle.dump(y_train, handle)
+
+    file_name = 'data/processed/y_test.pkl'
+    logger.info(f"Creating `{file_name}`")
+    with open(os.path.join(project_directory, file_name), 'wb') as handle:
+        pickle.dump(y_test, handle)
 
 
 if __name__ == '__main__':
