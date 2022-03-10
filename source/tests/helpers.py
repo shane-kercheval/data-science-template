c34c4462977e9e10e61e8a6b62300b94084edb27
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2022-03-10 15:44:58 -0800

    Update helpers.py

--- source/tests/helpers.py
+++ source/tests/helpers.py
@@ -2,7 +2,7 @@ import os
 from helpsk.utility import is_debugging
 
 
-def get_test_path(file_path) -> str:
+def get_test_file_path(file_path) -> str:
     """Returns the path to /tests folder, adjusting for the difference in the current working directory when
     debugging vs running from command line.
     """
