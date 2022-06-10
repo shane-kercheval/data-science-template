98315967a2de9c25d7b7f31c37ea1837f1e7d7f8
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2022-06-10 13:56:12 -0700

    Update experiments.py

--- source/scripts/experiments.py
+++ source/scripts/experiments.py
@@ -26,15 +26,22 @@ def run(input_directory: str,
         n_splits: int,
         n_repeats: int,
         score: str,
-        tracking_uri: str) -> str:
+        tracking_uri: str,
+        experiment_name: str = 'credit',
+        registered_model_name: str = 'credit_model',
+        required_performance_gain: float = 0.025) -> str:
     """
-    Logic For Running Experiments. The timestamp of the experiment is returned by the
-    function. The experiment results are saved to a sub-folder called `experiments__[timestamp]`.
+    Logic For Running Experiments. The timestamp of the experiment is returned by the function.
 
-    This function takes the full credit dataset, creates features, and separates the dataset into training
-    and test sets. It saves those datasets in `output_directory_data` for future reference.
-    The training set is used by BayesSearchCV to search for the best model and the test set is used to
-    evaluate the performance of the best model found. The results are saved in the output directory.
+    This function takes the full credit dataset in `input_directory`, and separates the dataset into training
+    and test sets. The training set is used by BayesSearchCV to search for the best model.
+    
+    The experiment results and corresponding training/tests sets  are saved to the mlflow server with the
+    corresponding `tracking_uri` provided. The runs will be in an experiment called `experiment_name`. 
+    The best model found by BayesSearchCV will be registered as `registered_model_name` with a new version
+    number. The best model will be put into production if it has a `required_performance_gain` percent
+    increase compared with the current model in production.
+
 
     Args:
         input_directory:
@@ -54,6 +61,14 @@ def run(input_directory: str,
         tracking_uri:
             MLFlow tracking URI e.g. http://localhost:1234
             Should match host/port in makefile `mlflow_server` command.
+        experiment_name:
+            The name of the experiment saved to MLFlow.
+        required_performance_gain:
+            The required perforance gain, as percentage, compared with current model in production, required
+            to put the new model (i.e. best model found by BayesSearchCV) into production. The default value
+            is `0.025` meaning if the best model found by BayesSearchCV is >=2.5% better than the current
+            model's performance, we will archive the current model in production and put the new model
+            in production.
     """
     log_func("experiments.run", params=dict(
         input_directory=input_directory,
@@ -76,9 +91,10 @@ def run(input_directory: str,
     y_full = label_binarize(y_full, classes=['good', 'bad']).flatten()
     x_train, x_test, y_train, y_test = train_test_split(x_full, y_full, test_size=0.2, random_state=42)
 
+    # set up MLFlow
     mlflow.set_tracking_uri(tracking_uri)
-    mlflow.set_experiment("credit")
-    mlflow.sklearn.autolog(registered_model_name='credit_model')
+    mlflow.set_experiment(experiment_name)
+    mlflow.sklearn.autolog(registered_model_name=registered_model_name)
     with Timer("Running Model Experiments (BayesSearchCV)"):
         with mlflow.start_run(run_name=timestamp,
                               description=timestamp,
@@ -111,20 +127,17 @@ def run(input_directory: str,
             # mlflow.log_params(bayes_search.best_params_)
 
             file_name = 'experiment'
-            # yaml_file_name = os.path.join(output_directory, f'{file_name}.yaml')
-            # model_file_name = os.path.join(output_directory, f'{file_name}_best_estimator.pkl')
             yaml_file_name = f'{file_name}.yaml'
             model_file_name = f'{file_name}_best_estimator.pkl'
 
-            # log_info(f"Saving results of BayesSearchCV to: `{yaml_file_name}`")
-            # results.to_yaml_file(yaml_file_name=yaml_file_name)
-            # mlflow.log_artifact(local_path=yaml_file_name)
             ml.log_ml_results(results=results, file_name=yaml_file_name)
             ml.log_pickle(obj=bayes_search.best_estimator_, file_name=model_file_name)
             ml.log_pickle(obj=x_train, file_name='x_train.pkl')
             ml.log_pickle(obj=x_test, file_name='x_test.pkl')
             ml.log_pickle(obj=y_train, file_name='y_train.pkl')
             ml.log_pickle(obj=y_test, file_name='y_test.pkl')
+
+    # if we find a model that outperforms the current model in production by `1.025`
 
     def transition_latest_model_to_production(ml_client: MlflowClient):
         """Get the latest version of credit_model and transitino stage to Production."""
@@ -147,11 +160,11 @@ def run(input_directory: str,
         # put latest model into production after archiving model currently in production
         assert len(production_model) == 1  # can only have one model in production
         production_model = production_model[0]
-        production_roc_auc = client.get_run(run_id=production_model.run_id).data.metrics['roc_auc']
-        if bayes_search.best_score_ > production_roc_auc * 1.025:
+        production_score = client.get_run(run_id=production_model.run_id).data.metrics[score]
+        if bayes_search.best_score_ > production_score * (1 + required_performance_gain):
             log_info(f"Archiving previous model "
-                     f"(version {production_model.version}; roc_auc {production_roc_auc}) and placing new "
-                     f"model into Production (roc_auc {bayes_search.best_score_})")
+                     f"(version {production_model.version}; {score} {production_score}) and placing new "
+                     f"model into Production ({score} {bayes_search.best_score_})")
             _ = client.transition_model_version_stage(
                 name=production_model.name,
                 version=production_model.version,
